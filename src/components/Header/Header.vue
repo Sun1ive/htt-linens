3e1f8d93f0b0e2b9f652985f1c3d5e79f77b54e8
Sun1ive <sunliveua@gmail.com>
2018-04-27 12:29:57 +0300

    update

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,6 +2,14 @@
   <header class="header">
     <wrapper>
       <Toolbar />
+      <div class="header__container">
+        <h1>Постельное белье и аксессуары</h1>
+        <h2>Мы сделаем ваш дом уютнее!</h2>
+        <div>
+          <Button class="btn mr-2">Перейти в каталог</Button>
+          <Button class="btn">Читать о компании</Button>
+        </div>
+      </div>
     </wrapper>
   </header>
 </template>
@@ -21,4 +29,15 @@ export default Vue.extend({
   background url('/header.png') no-repeat
   background-size cover
   min-height 700px
+  &__container
+    text-align center
+    padding-top 10%
+    h1
+      font-size 4.2rem
+      color $blue
+      font-family MullerBold
+    h2
+      font-size 3rem
+      color $grey
+      font-family Muller
 </style>
